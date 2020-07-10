75ecbcfd528f4a557a216e5269a6407e4894034f
ABHISHEK PATEL <abhishekpatel167@gmail.com>
2020-07-10 14:09:10 +0500

    Form Testing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -118,7 +118,7 @@
           </p>
 
           <div class="text-lg sm:text-lg mb-16">
-            <form
+            <!-- <form
               name="contact"
               method="POST"
               class="mb-12"
@@ -165,9 +165,8 @@
                   required
                 ></textarea>
               </div>
-              <div class="flex justify-end w-full">
-                <div data-netlify-recaptcha="true"></div>
-              </div>
+
+              <div data-netlify-recaptcha="true"></div>
 
               <div class="flex justify-end w-full">
                 <input
@@ -176,6 +175,38 @@
                   class="block bg-green-700 hover:bg-green-800 text-white text-sm font-semibold tracking-wide uppercase shadow rounded cursor-pointer px-6 py-3"
                 />
               </div>
+            </form>-->
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>
+                  Your Name:
+                  <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email:
+                  <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Role:
+                  <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                  </select>
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message:
+                  <textarea name="message"></textarea>
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
             </form>
           </div>
         </div>
