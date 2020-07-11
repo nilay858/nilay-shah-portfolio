92408f5cadf941b4d34c770f68426cd8cb74498b
ABHISHEK PATEL <abhishekpatel167@gmail.com>
2020-07-11 19:30:37 +0500

    Created Netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -68,61 +68,6 @@ module.exports = {
         cacheTime: 600000, // default
       },
     },
-    {
-      use: "gridsome-plugin-pwa",
-      options: {
-        // Service Worker Options
-        disableServiceWorker: false,
-        serviceWorkerPath: "service-worker.js",
-        cachedFileTypes: "js,json,css,html,png,jpg,jpeg,svg,gif",
-        disableTemplatedUrls: false, // Optional
-
-        // Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
-        manifestPath: "manifest.json",
-        title: "Nilay Shah",
-        startUrl: "/",
-        display: "standalone",
-        statusBarStyle: "default",
-        themeColor: "#666600",
-        backgroundColor: "#ffffff",
-        icon: "",
-        shortName: "NilayShah", // Optional
-        description: "Nilay Shah Portfolio", // Optional
-        categories: ["education"], // Optional
-        lang: "en-GB", // Optional
-        dir: "auto", // Optional
-        maskableIcon: true, // Optional
-        // screenshots: [                      // Optional
-        //     {
-        //         src: 'src/screenshot1.png',
-        //         sizes: '1280x720',
-        //         type: 'image/png',
-        //     },
-        // ],
-        gcmSenderId: undefined, // Optional
-
-        // Standard Meta Tags
-        svgFavicon: "favicon.png", // Optional. Requires favicon.ico fallback
-
-        // Microsoft Windows Meta Tags
-        msTileColor: "#666600", // Optional
-
-        // Apple MacOS Meta Tags
-        appleMaskIcon: "favicon.png", // Optional
-        appleMaskIconColor: "#666600", // Optional
-      },
-    },
-    {
-      use: "gridsome-plugin-manifest",
-      options: {
-        background_color: "#000000",
-        icon_path: "./src/favicon.png",
-        name: "Nilay Shah",
-        short_name: "NilayShah",
-        theme_color: "#FFFFFF",
-        lang: "en",
-      },
-    },
   ],
   templates: {
     Tag: "/tag/:id",
